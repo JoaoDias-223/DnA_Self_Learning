014dea2f332606a2a43d1d51c86d3dede1bd1284
João Vitor Dias <joao.dias.santos223@gmail.com>
2021-04-23 13:13:59 -0300

    feat(): add pokemon repository

--- pokeDex/src/main/kotlin/com/slearning/pokedex/controller/PokemonRepository.kt
+++ pokeDex/src/main/kotlin/com/slearning/pokedex/controller/PokemonRepository.kt
@@ -1,12 +1,14 @@
 package com.slearning.pokedex.controller
 
-import com.slearning.pokedex.model.bodyTemplates.PokemonDTO
-import com.slearning.pokedex.model.bodyTemplates.SkillDTO
+import com.slearning.pokedex.databases.PokemonDatabase
+import com.slearning.pokedex.databases.SkillDatabase
+import com.slearning.pokedex.model.dtos.PokemonDTO
+import com.slearning.pokedex.model.dtos.SkillDTO
 import org.springframework.stereotype.Component
 
 @Component
-class PokemonController(private val pokemonDatabaseController: DatabaseController,
-                        private val skillDatabaseController: DatabaseController
+class PokemonRepository(private val pokemonDatabase: PokemonDatabase,
+                        private val skillDatabase: SkillDatabase
 ) {
 
     companion object {
@@ -14,18 +16,53 @@ class PokemonController(private val pokemonDatabaseController: DatabaseControlle
         const val DUPLICATE_POKEMON_ERROR = 1
         const val UNREGISTERED_SKILL_ERROR = 2
         const val TYPE_AND_SKILL_MISMATCH = 3
+        const val UNREGISTERED_POKEMON_ERROR = 5
     }
 
-    fun createPokemon(newPokemonData: PokemonDTO): Int {
-        val pokemons: MutableMap<String, PokemonDTO> = pokemonDatabaseController.getMutableMapOfDeserializedData()
-        val skills: MutableMap<String, SkillDTO> = skillDatabaseController.getMutableMapOfDeserializedData()
+    fun createPokemon(pokemonData: PokemonDTO): Int {
+        val pokemons: MutableMap<String, PokemonDTO> = pokemonDatabase.getData()
+        val skills: MutableMap<String, SkillDTO> = skillDatabase.getData()
 
         return when {
+            isPokemonAlreadyRegistered(pokemonData, pokemons) -> DUPLICATE_POKEMON_ERROR
+            arePokemonSkillsUnregisteredInDB(pokemonData, skills) -> UNREGISTERED_SKILL_ERROR
+            arePokemonTypeAndSkillsNotMatching(pokemonData) -> TYPE_AND_SKILL_MISMATCH
+            else -> {
+                pokemonDatabase.insertData(pokemonData.hashCode().toString(), pokemonData)
+                OK
+            }
+        }
+    }
+
+    fun getPokemons(): MutableMap<String, PokemonDTO> {
+        return pokemonDatabase.getData()
+    }
+
+    fun getPokemonById(id: String): PokemonDTO? {
+        return pokemonDatabase.getData()[id]
+    }
+
+    fun updatePokemonById(id: String, newPokemonData: PokemonDTO): Int {
+        val pokemons: MutableMap<String, PokemonDTO> = pokemonDatabase.getData()
+        val skills: MutableMap<String, SkillDTO> = skillDatabase.getData()
+
+        return when {
+            !pokemonDatabase.hasId(id) -> UNREGISTERED_POKEMON_ERROR
             isPokemonAlreadyRegistered(newPokemonData, pokemons) -> DUPLICATE_POKEMON_ERROR
             arePokemonSkillsUnregisteredInDB(newPokemonData, skills) -> UNREGISTERED_SKILL_ERROR
             arePokemonTypeAndSkillsNotMatching(newPokemonData) -> TYPE_AND_SKILL_MISMATCH
             else -> {
-                pokemonDatabaseController.insertNewDeserializedData(newPokemonData)
+                pokemonDatabase.insertData(id, newPokemonData)
+                OK
+            }
+        }
+    }
+
+    fun deletePokemonById(id: String): Int {
+        return when {
+            !pokemonDatabase.hasId(id) -> UNREGISTERED_POKEMON_ERROR
+            else -> {
+                pokemonDatabase.deleteById(id)
                 OK
             }
         }
