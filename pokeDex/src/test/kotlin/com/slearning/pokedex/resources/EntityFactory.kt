50ade3195beebbffc01fbab2f2f9ddc203b46350
João Vitor Dias <joao.dias.santos223@gmail.com>
2021-06-06 21:53:44 -0300

    feat(SerializerTests): add more unit tests
    Signed-off-by: João Vitor Dias <joao.dias.santos223@gmail.com>

---
+++ pokeDex/src/test/kotlin/com/slearning/pokedex/resources/EntityFactory.kt
@@ -0,0 +1,37 @@
+package com.slearning.pokedex.resources
+
+import com.slearning.pokedex.model.Pokemon
+import com.slearning.pokedex.model.Skill
+import com.slearning.pokedex.model.Type
+
+class EntityFactory {
+    companion object {
+        fun createPokemon(
+            id: String = "POKEMON_ID",
+            name: String = "POKEMON_NAME",
+            description: String = "POKEMON_DESCRIPTION",
+            types: Set<Type> = setOf(createType(id="POKEMON_TYPE_ID", name="POKEMON_TYPE_NAME")),
+            skills: Set<Skill> = setOf(createSkill())
+        ): Pokemon {
+            return Pokemon(id, name, description, types, skills)
+        }
+
+        fun createType(
+            id: String = "TYPE_ID",
+            name: String = "TYPE_NAME"
+        ): Type {
+            return Type(id, name)
+        }
+
+        fun createSkill(
+            id: String = "SKILL_ID",
+            name: String = "SKILL_NAME",
+            description: String = "SKILL_DESCRIPTION",
+            types: Set<Type> = setOf(createType(id="SKILL_TYPE_ID", name="SKILL_TYPE_NAME")),
+            action: String = "SKILL_ACTION",
+            actionPoints: Int = 999999
+        ): Skill {
+            return Skill(id, name, description, types, action, actionPoints)
+        }
+    }
+}
--- pokeDex/src/test/kotlin/com/slearning/pokedex/resources/JsonFactory.kt
+++ pokeDex/src/test/kotlin/com/slearning/pokedex/resources/JsonFactory.kt
@@ -2,27 +2,59 @@ package com.slearning.pokedex.resources
 
 import com.slearning.pokedex.controller.Serializer.toJson
 import com.slearning.pokedex.model.Skill
-import com.slearning.pokedex.model.Type
 
 class JsonFactory {
     companion object{
         fun createPokemon(
-            id: String,
+            id: String = "POKEMON_ID",
             name: String = "POKEMON_NAME",
             description: String = "POKEMON_DESCRIPTION",
-            types: Set<Type> = setOf(),
-            skills: Set<Skill> = setOf()
-        ): String
-        {
-            return """
-                {
-                    "id": "$id",
-                    "name": "$name",
-                    "description": "$description",
-                    "types":${types.toJson()},
-                    "skills":${skills.toJson()}
+            types: Set<String> = setOf(createType(id="POKEMON_TYPE_ID", name="POKEMON_TYPE_NAME")),
+            skills: Set<String> = setOf(createSkill())
+        ): String {
+            val typesStr = getFormattedStringFromSet(types)
+            val skillsStr = getFormattedStringFromSet(skills)
+
+            return "{\"id\":\"$id\",\"name\":\"$name\",\"description\":\"$description\",\"types\":[$typesStr]," +
+                    "\"skills\":[$skillsStr]}"
+        }
+
+        fun createType(
+            id: String = "TYPE_ID",
+            name: String = "TYPE_NAME"
+        ): String {
+            return "{\"id\":\"$id\",\"name\":\"$name\"}"
+        }
+
+        fun createSkill(
+            id: String = "SKILL_ID",
+            name: String = "SKILL_NAME",
+            description: String = "SKILL_DESCRIPTION",
+            types: Set<String> = setOf(createType(id = "SKILL_TYPE_ID", name = "SKILL_TYPE_NAME")),
+            action: String = "SKILL_ACTION",
+            actionPoints: Int = 999999
+        ): String {
+            val typesStr = getFormattedStringFromSet(types)
+
+            return "{\"id\":\"$id\",\"name\":\"$name\",\"description\":\"$description\",\"types\":[$typesStr]," +
+                    "\"action\":\"$action\",\"actionPoints\":$actionPoints}"
+        }
+
+        private fun getFormattedStringFromSet(set: Set<String>): String {
+            var string = ""
+            var setSize = set.size
+
+            set.forEach { element ->
+                string += element
+
+                if (setSize - 1 != 0) {
+                    string += ","
                 }
-            """.trimIndent()
+                setSize -= 1
+
+            }
+
+            return string
         }
     }
 }
