58841234be83d28829d39de924c4a524e45f2624
João Vitor Dias <joao.dias.santos223@gmail.com>
2021-06-09 10:07:28 -0300

    feat(PokemonRepositoryControllerCodes): create enum to encapsulate the returns of successful and unsuccessful operations
    Signed-off-by: João Vitor Dias <joao.dias.santos223@gmail.com>

---
+++ pokeDex/src/main/kotlin/com/slearning/pokedex/controller/resources/PokemonRepositoryControllerCodes.kt
@@ -0,0 +1,10 @@
+package com.slearning.pokedex.controller.resources
+
+enum class PokemonRepositoryControllerCodes {
+    OK,
+    DUPLICATE_POKEMON_ERROR,
+    UNREGISTERED_SKILL_ERROR,
+    TYPE_AND_SKILL_MISMATCH,
+    UNREGISTERED_POKEMON_ERROR,
+    UNKNOWN_ERROR
+}
